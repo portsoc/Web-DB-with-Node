e7c7ed25605e3949a127e25a26f2086da6614098
Jacek Kopecky <jacek@jacek.cz>
2015-02-04 22:18:25 +0000

    regular commit, much more functionality

--- server.js
+++ server.js
@@ -9,6 +9,7 @@
 var express = require('express')
 var fs = require('fs')
 var bodyParser = require('body-parser');
+var auth = require('basic-auth');
 
 var app = express()
 app.set('strict routing', true)
@@ -20,13 +21,24 @@ app.use(bodyParser.json({limit: 4096}))
 
 var apiKey = 'orewgthwoetgoirwejgboerigqt';
 
+function checkApiKey (req, res, next) {
+    var creds = auth(req);
+    if (!creds || creds.name !== apiKey) {
+        res.set('WWW-Authenticate', 'Basic realm=API Key required');
+        return res.status(401).send('API Key required');
+    }
+    next();
+}
+
 
 var categories = {
       categories: [
         { title: "Cameras",
           productsURL: "/api/categories/cam/" },
         { title: "Laptops",
-          productsURL: "/api/categories/laptop/" }
+          productsURL: "/api/categories/laptop/" },
+        { title: "Phones",
+          productsURL: "/api/categories/phone/" }
       ]
     };
 
@@ -48,6 +60,13 @@ products['cam'] = {
           description: 'Body (no lenses), 40MPix',
           stock: 2,
           vendor: 'BigShop Inc.'
+        },
+        gunonp30e: {
+          title: 'Gunon P30E',
+          price: 399.99,
+          description: 'Body (no lenses), 30MPix, discontinued',
+          stock: 0,
+          vendor: 'BigShop Inc.'
         }
       }
     };
@@ -62,11 +81,38 @@ products['laptop'] = {
           vendor: 'BigShop Inc.'
         },
         leonvo386: {
-          title: 'Leonvo 386',
+          title: 'Leonvo Classic 386',
           price: 299.99,
           description: '13.3" flexible with Doors',
           stock: 73,
           vendor: 'BigShop Inc.'
+        }
+      }
+    };
+
+products['phone'] = {
+      category: 'Phones',
+      products: {
+        lexus1: {
+          title: 'Lexus 1',
+          price: 219.99,
+          description: 'discontinued',
+          stock: 0,
+          vendor: 'Kaboodle Inc.'
+        },
+        lexus2: {
+          title: 'Lexus 5',
+          price: 299.99,
+          description: 'better, almost discontinued',
+          stock: 7,
+          vendor: 'Kaboodle Inc.'
+        },
+        iph6: {
+          title: 'flyPhone 6',
+          price: 7399.90,
+          description: 'not cheap but desirable',
+          stock: 137,
+          vendor: 'Oranges Pears etc. Ltd'
         }
       }
     };
@@ -101,6 +147,14 @@ var orders = [
 
 
 app.get('/api/', function(req, res) { res.redirect('/api'); });
+
+// require api key for api calls
+// it's here after setting /api/ so that we don't require the key for /api/
+app.use('/api/*', checkApiKey);
+
+app.use('/api/*', delay);
+
+
 app.get('/api/categories', function(req, res) { res.redirect(req.url + '/'); });
 app.get('/api/categories/:id', function(req, res) { res.redirect(req.url + '/'); });
 
@@ -139,6 +193,15 @@ app.post('/api/orders/', function(req, res) {
     order.dispatched = false;
     var orderNo = orders.push(order) - 1;
     res.location('/api/orders/' + orderNo).status(201).send(order);
+
+    console.log("received order " + orderNo);
+
+    // dispatch the order in a minute
+    setTimeout(dispatch, 60000);
+    function dispatch() {
+        order.dispatched = true;
+        console.log("dispatched order " + orderNo);
+    }
 });
 
 app.get('/api/orders/:id', function(req, res) {
@@ -157,4 +220,9 @@ app.listen(8088);
 
 function notImplemented(req, res) {
     res.status(501).send("this functionality is envisioned but not implemented yet\n");
+}
+
+function delay(req, res, next) {
+    next();
+    // setTimeout(next, 1000);
 }
