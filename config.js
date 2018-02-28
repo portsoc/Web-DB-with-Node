d8a480e1c499d85b9ba05043cebe13c67ce3a7fc
Jacek Kopecky <jacek@jacek.cz>
2018-02-28 21:28:42 +0000

    :shirt:

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    apiKey: 'orewgthwoetgoirwejgboerigqt',
-    mysql: {
-        host:'localhost',
-        user:'dbprinHappyShop',
-        password:'weiothbgdls',
-        database: 'dbprinHappyShop'
-    },
-    expressStatic: {
-        maxAge: 5*60*1000, /* fiveMinutes */
-        extensions: [ "html" ]
-    },
-    bodyParser_JSON: {
-        limit: 4096
-    },
-    port: process.env.PORT || 8080,
-    apiCallDelay: 1000
-}
+  apiKey: 'orewgthwoetgoirwejgboerigqt',
+  mysql: {
+    host:'localhost',
+    user:'dbprinHappyShop',
+    password:'weiothbgdls',
+    database: 'dbprinHappyShop',
+  },
+  expressStatic: {
+    maxAge: 5*60*1000, /* fiveMinutes */
+    extensions: ['html'],
+  },
+  bodyParser_JSON: {
+    limit: 4096,
+  },
+  port: process.env.PORT || 8080,
+  apiCallDelay: 1000,
+};
